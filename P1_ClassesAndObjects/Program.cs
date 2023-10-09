f0173883de40a8db3d1796499b35b819e23f9f9f
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-09 15:39:18 +0200

    Finished both exercises in P1_ClassesAndObjects

--- P1_ClassesAndObjects/Program.cs
+++ P1_ClassesAndObjects/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿/*
+Person[] people = new Person[3];
+
+for (var i = 0; i < people.Length; i++)
+{
+    people[i] = new Person();
+    Console.WriteLine(people[i]);
+}
+
+public class Person
+{
+    
+}
+*/
+
+
+
+
+/* Exercise 2
+Person printPerson = new Person();
+Console.WriteLine(printPerson);
+
+Animal printAnimal = new Animal();
+Console.WriteLine(printAnimal);
+
+Car printCar = new Car();
+Console.WriteLine(printCar);
+
+public class Person
+{
+    
+}
+
+public class Animal
+{
+    
+}
+
+public class Car
+{
+    
+}
+*/
+
