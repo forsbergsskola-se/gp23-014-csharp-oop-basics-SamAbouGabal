b1a624317bb6f7d9a3593044cf3d93999e884ad7
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-11 10:28:38 +0200

    Changed variable name in P3_2People

--- P3_2People/Program.cs
+++ P3_2People/Program.cs
@@ -1,15 +1,15 @@
 ﻿using P3_2People;
 
-Person[] person = new Person[3];
-for (var i = 0; i < person.Length; i++)
+Person[] people = new Person[3];
+for (var i = 0; i < people.Length; i++)
 {
-    person[i] = new Person();
+    people[i] = new Person();
 }
-for (var i = 0; i < person.Length; i++)
+for (var i = 0; i < people.Length; i++)
 {
-    person[i].Name = Console.ReadLine();
+    people[i].Name = Console.ReadLine();
 }
-for (var i = 0; i < person.Length; i++)
+for (var i = 0; i < people.Length; i++)
 {
-    person[i].Greeting();
+    people[i].Greeting();
 }
