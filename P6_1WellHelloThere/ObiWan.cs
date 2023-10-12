705eb8c72d1192e3aa0d34e0d225ae05bbaa55c6
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-12 14:25:41 +0200

    Finished P6_1WellHelloThere

---
+++ P6_1WellHelloThere/ObiWan.cs
@@ -0,0 +1,6 @@
+namespace P6_1WellHelloThere;
+
+public class ObiWan
+{
+    public Lightsaber Weapon;
+}
--- P6_1WellHelloThere/Program.cs
+++ P6_1WellHelloThere/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using P6_1WellHelloThere;
+
+ObiWan obiWan = new ObiWan();
+Console.WriteLine(obiWan);
+Console.WriteLine(obiWan.Weapon);
+obiWan.Weapon = new Lightsaber();
+Console.WriteLine(obiWan.Weapon);
